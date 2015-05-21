04374b5ca705d22abe77e32f6b635a9d799be7e0
Sharon <sharon.peishan.kennedy@gmail.com>
2015-05-21 20:58:07 +0200

    added faq page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'site#index'
+  get '/faq', to: 'site#faq'
   resources :originators
   resources :applicants
   match '/404', to: 'errors#file_not_found', via: :all
