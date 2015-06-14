f69bf3049853d7deff68c859f201901ba3fe9c95
Sharon <sharon.peishan.kennedy@gmail.com>
2015-06-14 12:24:55 +0200

    changed .com to .org in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,5 @@ Rails.application.configure do
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
-  config.action_mailer.default_url_options = { :host => 'www.meetandmultiply.com' }
+  config.action_mailer.default_url_options = { :host => 'www.meetandmultiply.org' }
 end
